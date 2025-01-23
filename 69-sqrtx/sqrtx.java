c6fc349f0b2e6df54efa76c3cc3556bf1e792ec9
Pooja Salunke <128277291+pooja9008@users.noreply.github.com>
2025-01-23 14:24:31 +0530

    Time: 1 ms (87.25%) | Memory: 40.8 MB (67.56%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.Math;
+class Solution {
+    public static int mySqrt(int x) {
+        int l = 0;
+        int r = x;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            long square = (long) m * m;
+            if (square == x) {
+                return m;
+            } else if (square < x) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return r;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.print(mySqrt(x));
+    }
+}
