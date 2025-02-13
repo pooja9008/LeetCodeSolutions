6c738605928e26ece688825aa64787deae2eb7d1
Pooja Salunke <128277291+pooja9008@users.noreply.github.com>
2025-02-13 22:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (65.43%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,24 @@
+class Solution {
+     public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int x : nums) {
+            if (x != val) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=sc.nextInt();
+        }
+        int val=sc.nextInt();
+        int ar=removeElement(nums,val);
+        
+            System.out.print(ar);
+       
+    }
+}
